bf31c89e189c4f915c1f59d2f98db521363dcb33
wangnick <github@semail.wangnick.de>
2014-05-27 22:42:15 +0200

    Copyrights added and license withdrawn

--- README.txt
+++ README.txt
@@ -1,5 +1,3 @@
-Radio-Bootloader for Attiny84A and nRF24L01+, developed using Atmel Studio.
-See http://s.wangnick.de/doku.php?id=betriebsstundenzaehler for target hardware.
-(C) Copyright 2014 Sebastian Wangnick.
-Usage under "CC Attribution-Noncommercial-Share Alike 3.0 Unported" as described in http://creativecommons.org/licenses/by-nc-sa/3.0/ is granted.
-Separate license conditions apply for the files "nRF24L01.h" and "ds.h".
+Mirfboot - Radio-Bootloader for Attiny84A and nRF24L01+, developed using Atmel Studio.
+(C) Copyright 2014 Sebastian Wangnick (except where noted otherwise).
+See http://s.wangnick.de/doku.php?id=betriebsstundenzaehler for design details.
--- mirfboot.c
+++ mirfboot.c
@@ -1,7 +1,7 @@
-// Radio-Bootloader for Attiny84A and nRF24L01+, developped using Atmel Studio.
-// See http://s.wangnick.de/doku.php?id=betriebsstundenzaehler for target hardware.
+// Mirfboot - Radio-Bootloader for Attiny84A and nRF24L01+, developed using Atmel Studio.
 // (C) Copyright 2014 Sebastian Wangnick.
-// Usage under "CC Attribution-Noncommercial-Share Alike 3.0 Unported" as described in http://creativecommons.org/licenses/by-nc-sa/3.0/ is granted.
+// See http://s.wangnick.de/doku.php?id=betriebsstundenzaehler for design details.
+
 //
 //                                                MISO    MOSI
 //                                nRF24L01 MOSI - DO      DI - nRF24L01 MISO
